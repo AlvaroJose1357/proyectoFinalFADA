30599031faa671d677e5ebb636dffe857b765b2f
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-05-26 14:11:55 -0500

    botones ubicados

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -3,7 +3,7 @@ import tkinter
 #-------Ventana-------
 ventana = tkinter.Tk()
 ventana.title("Prueba")
-ventana.geometry("700x500")
+ventana.geometry("800x500")
 ventana.resizable(0,0)
 
 #------botones
@@ -18,15 +18,15 @@ def eliminar():
 
 botonAbrir = tkinter.Button(ventana, text="Invocar saludo", command= abrir, fg="red")
 botonAbrir.pack()
-botonAbrir.place(x= 100, y= 50) #ubicar boton dentro de ventana
+botonAbrir.place(x= 5, y= 0) #ubicar boton dentro de ventana
 
 botonGuardar = tkinter.Button(ventana, text="Invocar saludo", command= guardar, fg="red")
 botonGuardar.pack()
-botonGuardar.place(x= 350, y= 50)
+botonGuardar.place(x= 95, y= 0)
 
 botonEliminar = tkinter.Button(ventana, text="Invocar saludo", command= eliminar, fg="red")
 botonEliminar.pack()
-botonEliminar.place(x= 600, y= 50)
+botonEliminar.place(x= 185, y= 0)
 
 
 
