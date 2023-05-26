757da4b8e21865248e825505edbecfd23896bbb8
DayB6 <deybiperea666@gmail.com>
2023-05-26 15:55:38 -0500

    si

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -1,13 +1,35 @@
 import tkinter
 
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, asksaveasfile
+
 #-------Ventana-------
-ventana = tkinter.Tk()
-ventana.title("Prueba")
+if __name__ == '__main__':
+
+    ventana = Tk()
+ventana.title("Prueba: Bloc de Notas")
 ventana.geometry("800x500")
 ventana.resizable(0,0)
+ventana.mainloop()
 
+#-----------Menubar-----------
+
+menubar = Menu(ventana)
+ventana.config(menu=menubar)
+
+archivo = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Archivo", menu=archivo)
+
+archivo.add_command(label="Nuevo", command=nuevo)
+archivo.add_command(label="Abrir     ", command=abrir)
+
+archivo.add_command(label="Guardar     ", command=guardar)
+
+archivo.add_command(label="Salir     ", command=ventana.quit)
 #------botones
-def abrir():
+'''def abrir():
     tkinter.Label(ventana, text="Aqui va abrir").pack()
 
 def guardar():
@@ -26,7 +48,7 @@ botonGuardar.place(x= 95, y= 0)
 
 botonEliminar = tkinter.Button(ventana, text="Invocar saludo", command= eliminar, fg="red")
 botonEliminar.pack()
-botonEliminar.place(x= 185, y= 0)
+botonEliminar.place(x= 185, y= 0)'''
 
 
 
