b0b5185a331ce81cda97990ef03a167b650d4642
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-05-26 14:53:38 -0500

    Ventana y boton

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -1,5 +1,29 @@
 import tkinter
 
-print("hello word")
+#-------Ventana-------
+ventana = tkinter.Tk()
+ventana.title("Prueba")
+ventana.geometry("700x500")
+ventana.resizable(0,0)
 
-tkinter._test()
+#------botones
+def saludo():
+    tkinter.Label(ventana, text="hello word").pack
+
+def salir():
+    ventana.destroy
+
+boton = tkinter.Button(ventana, text="Invocar saludo", command= saludo, fg="red")
+boton.pack()
+boton.place(x= 300, y= 250) #ubicar boton dentro de ventana
+
+
+
+
+
+
+ventana.mainloop()
+
+
+
+#tkinter._test()
